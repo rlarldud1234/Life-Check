b7e7db80e29cb8944a9798739d2ae9c0317f8582
rlarldud1234 <rlarldud6482@naver.com>
2025-02-10 16:09:32 +0900

    new : color 지정

--- lib/assets/color.dart
+++ lib/assets/color.dart
@@ -1,3 +1,6 @@
-enum Colors { 
-  primary500 = Color('316BFF')
+import 'dart:ui';
+
+abstract class AppColor {
+  static const primary500 = Color.fromRGBO(49, 107, 255, 100);
+  static const gray700 = Color.fromRGBO(99, 99, 102, 100);
 }
