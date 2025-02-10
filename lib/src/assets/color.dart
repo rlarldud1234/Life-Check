a757662097c2fa5631a8ae6871b1a28b33f6b984
rlarldud1234 <rlarldud6482@naver.com>
2025-02-10 19:52:02 +0900

    error : opacity 1.0으로 수정하여 에러 수정

--- lib/src/assets/color.dart
+++ lib/src/assets/color.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
 abstract class AppColor {
-  static const primary500 = Color.fromRGBO(49, 107, 255, 100);
-  static const gray700 = Color.fromRGBO(99, 99, 102, 100);
+  static const primary500 = Color.fromRGBO(49, 107, 255, 1.0);
+  static const gray700 = Color.fromRGBO(99, 99, 102, 1.0);
 }
