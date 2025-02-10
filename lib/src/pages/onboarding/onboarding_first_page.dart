584f311363181bc015c5f6bb98a664ba02d83908
rlarldud1234 <rlarldud6482@naver.com>
2025-02-10 19:52:18 +0900

    update : OnboardingFirstPage

--- lib/src/pages/onboarding/onboarding_first_page.dart
+++ lib/src/pages/onboarding/onboarding_first_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:life_check/widgets/onboarding/onboarding_widget.dart';
+import 'package:life_check/src/pages/onboarding/onboarding_second_page.dart';
+import 'package:life_check/src/widgets/onboarding/onboarding_widget.dart';
 
 class OnboardingFirstPage extends StatelessWidget {
   const OnboardingFirstPage({super.key});
@@ -7,9 +8,13 @@ class OnboardingFirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return OnboardingWidget(
+      image: "https://sitem.ssgcdn.com/47/78/22/item/1000031227847_i1_750.jpg",
       title: "개인 맞춤형 운동 계획",
       content: "운동, 식단 등을 내 목표에 맞게 계획 해요!",
-      onPressed: () {},
+      onPressed: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) => OnboardingSecondPage()));
+      },
     );
   }
 }
