66d535008b1b4f16175e475579e9e413da3f3936
rlarldud1234 <rlarldud6482@naver.com>
2025-02-10 19:52:53 +0900

    new : OnboardingThirdPage

---
+++ lib/src/pages/onboarding/onboarding_third_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:life_check/src/widgets/onboarding/onboarding_widget.dart';
+
+class OnboardingThirdPage extends StatelessWidget {
+  const OnboardingThirdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingWidget(
+      image: "https://sitem.ssgcdn.com/47/78/22/item/1000031227847_i1_750.jpg",
+      title: "진행 상황 추적",
+      content: "운동 진행 상황을 기록하고 확인하세요!",
+      onPressed: () {
+        // Navigator.of(context).push(MaterialPageRoute(
+        //     builder: (BuildContext context) => ()));
+      },
+    );
+  }
+}
