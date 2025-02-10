74edb3f5ae48d6f4d17113c619a202c2a95b4665
rlarldud1234 <rlarldud6482@naver.com>
2025-02-10 18:29:40 +0900

    move : src 파일을 생성 및 파일 구조 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:life_check/pages/onboarding/onboarding_first_page.dart';
+import 'package:life_check/src/pages/onboarding/onboarding_first_page.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -24,64 +24,19 @@ class _MyAppState extends State<MyApp> {
   }
 
   void initialization() async {
-    // 여기에 초기 데이터 로딩이나 필요한 설정을 수행
     await Future.delayed(const Duration(seconds: 2)); // 예시로 2초 대기
 
-    // 스플래시 스크린 제거
     FlutterNativeSplash.remove();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Life Check',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const OnboardingFirstPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
