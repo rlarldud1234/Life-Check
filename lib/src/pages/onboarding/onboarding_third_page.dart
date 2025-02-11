8fe1a28cd952143e6cdc366786d9c73a4a9dd298
rlarldud1234 <rlarldud6482@naver.com>
2025-02-12 08:29:05 +0900

    update: navigate to onboardingFourthPage

--- lib/src/pages/onboarding/onboarding_third_page.dart
+++ lib/src/pages/onboarding/onboarding_third_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:life_check/src/pages/onboarding/onboarding_fourth_page.dart';
 import 'package:life_check/src/widgets/onboarding/onboarding_widget.dart';
 
 class OnboardingThirdPage extends StatelessWidget {
@@ -9,10 +10,10 @@ class OnboardingThirdPage extends StatelessWidget {
     return OnboardingWidget(
       image: "https://sitem.ssgcdn.com/47/78/22/item/1000031227847_i1_750.jpg",
       title: "진행 상황 추적",
-      content: "운동 진행 상황을 기록하고 확인하세요!",
+      content: "운동 진행 상황을 기록하고 확인해요!",
       onPressed: () {
-        // Navigator.of(context).push(MaterialPageRoute(
-        //     builder: (BuildContext context) => ()));
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) => OnboardingFourthPage()));
       },
     );
   }
